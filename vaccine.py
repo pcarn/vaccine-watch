30b3378beee6f4f6a13e0e9b32aba4d158298a2f
Peter Carnesciali <pcarn9@gmail.com>
2021-02-26 23:42:04 -0600

    lower true values

--- vaccine.py
+++ vaccine.py
@@ -2,10 +2,10 @@ import os
 
 from clinics.hyvee import get_available_hyvees
 
-TRUE_VALUES = ["true", "True", "TRUE", "1"]
+TRUE_VALUES = ["true", "1"]
 
 
 def check_for_appointments():
-    if os.environ["ENABLE_HYVEE"] in TRUE_VALUES:
+    if os.environ["ENABLE_HYVEE"].lower() in TRUE_VALUES:
         for clinic in get_available_hyvees():
             print(clinic["id"])
